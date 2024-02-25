d8fd4ea843004ba0cb0a6c3fdebb3cf5331665be
Yusif <yusiftrull@gmail.com>
2024-02-25 21:53:14 +0200

    add fun (new product)

--- product/views.py
+++ product/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts          import render, get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.response   import Response
-from rest_framework.pagination import PageNumberPagination
+from django.shortcuts                   import render, get_object_or_404
+from rest_framework.decorators          import api_view, permission_classes
+from rest_framework.response            import Response
+from rest_framework.pagination          import PageNumberPagination
+from rest_framework.permissions         import IsAuthenticated
 
-from product.filters           import *
-from product.models            import *
-from product.serializers       import *
+
+from product.filters                    import *
+from product.models                     import *
+from product.serializers                import *
 # Create your views here.
 
 @api_view(['GET'])
@@ -50,3 +52,19 @@ def get_by_id_product(request,pk):
     }
     
     return Response(context)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def new_product(request):
+    data       = request.data 
+    serializer = ProductSerializer(data=data)
+    
+    if serializer.is_valid():
+        product = Product.objects.create(**data, user=request.user)
+        res = ProductSerializer(product, many=False)
+        
+        return Response({"product":res.data})
+    else:
+        return Response(serializer.errors)
+    
+    
