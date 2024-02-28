c35f8ce0057c4573cd6c9114339343422e9a6d43
Yusif <yusiftrull@gmail.com>
2024-02-28 17:14:09 +0200

    add update product and create product fun

--- product/views.py
+++ product/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts          import render, get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.response   import Response
-from rest_framework.pagination import PageNumberPagination
+from django.shortcuts           import get_object_or_404,render
+from rest_framework.decorators  import api_view,permission_classes
+from rest_framework.response    import Response
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework             import status
+from rest_framework.pagination  import PageNumberPagination
+from django.db.models           import Avg
 
 from product.filters           import *
 from product.models            import *
@@ -50,3 +53,40 @@ def get_by_id_product(request,pk):
     }
     
     return Response(context)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated,IsAdminUser])
+def new_product(request):
+    data = request.data
+    serializer = ProductSerializer(data = data)
+
+    if serializer.is_valid():
+        product = Product.objects.create(**data,user=request.user)
+        res = ProductSerializer(product,many=False)
+
+        return Response({"product":res.data})
+    else:
+        return Response(serializer.errors)
+    
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated,IsAdminUser])
+def update_product(request,pk):
+    product = get_object_or_404(Product,id=pk)
+
+    if product.user != request.user:
+        return Response({"error":"Sorry you can not update this product"}
+                        , status=status.HTTP_403_FORBIDDEN)
+    
+    product.name = request.data['name']
+    product.description = request.data['description']
+    product.price = request.data['price']
+    product.brand = request.data['brand']
+    product.category = request.data['category']
+    product.ratings = request.data['ratings']
+    product.stock = request.data['stock']
+
+    product.save()
+    serializer = ProductSerializer(product,many=False)
+    return Response({"product":serializer.data})
