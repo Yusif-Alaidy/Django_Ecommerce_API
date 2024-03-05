276a912b55a71eecf377fd3476642e890c6ea49b
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2024-03-05 13:34:08 +0200

    .

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .           import views
 
 
 urlpatterns = [
     path('orders/new/', views.new_order,name='new_order'),
-    path('orders/', views.get_orders,name='get_orders'),
+    path('orders/',     views.get_orders,name='get_orders'),
 ]
