544ed35e38286443672c27d548e1bd9a2aa658b7
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2024-03-05 13:39:24 +0200

    .

--- order/urls.py
+++ order/urls.py
@@ -3,7 +3,7 @@ from .           import views
 
 
 urlpatterns = [
-    path('orders/new/', views.new_order,name='new_order'),
-    path('orders/',     views.get_orders,name='get_orders'),
+    path('orders/new/',      views.new_order,name='new_order'),
+    path('orders/',          views.get_orders,name='get_orders'),
     path('orders/<str:pk>/', views.get_order,name='get_order'), 
 ]
